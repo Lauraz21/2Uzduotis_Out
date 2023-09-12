5efaf80e5f07b54505b20d437913a3023eb92575
Laura <Laura@DESKTOP-BESSB6E>
2023-09-12 20:04:31 +0300

    Add project files.

---
+++ 2Uzduotis_Out/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _2Uzduotis_Out
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GetUserData(out string name, out string surname);
+            Console.WriteLine(name + " " + surname);
+        }
+        public static void GetUserData(out string name, out string surname)
+        {
+            name = Console.ReadLine();
+            surname = Console.ReadLine();
+        }
+    }
+}
